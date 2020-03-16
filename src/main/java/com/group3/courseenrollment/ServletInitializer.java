0dcaa6d0fa4a4ec95752dc4ce4473c62424205e9
sarfophil <philsarfogh@gmail.com>
2020-03-16 18:56:52 -0500

    initial commit

---
+++ src/main/java/com/group3/courseenrollment/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.group3.courseenrollment;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CourseEnrollmentApplication.class);
+	}
+
+}
---
+++ src/test/java/com/group3/courseenrollment/CourseEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.courseenrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
