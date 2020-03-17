b77f9a715718cd38e03ff30ae71dd26aa0e4191e
sarfophil <philsarfogh@gmail.com>
2020-03-17 02:40:34 -0500

    spring security and jwt integration

--- src/main/java/com/group3/courseenrollment/CourseEnrollmentApplication.java
+++ src/main/java/com/group3/courseenrollment/CourseEnrollmentApplication.java
@@ -1,13 +1,24 @@
 package com.group3.courseenrollment;
 
+import com.group3.courseenrollment.configuration.ApplicationProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class CourseEnrollmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseEnrollmentApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/com/group3/courseenrollment/domain/Credential.java
@@ -0,0 +1,22 @@
+package com.group3.courseenrollment.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class Credential {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Role> roles = new ArrayList<>();
+
+
+}
